422355af354c2d14faa7af2a1cabe50b4b9a629a
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-24 16:23:31 +0800

    thonnypico 2022.10.5

---
+++ led_threading.py
@@ -0,0 +1,22 @@
+from machine import Pin,Timer
+import time
+
+led=Pin(25,Pin.OUT)
+led14=Pin(14,Pin.OUT)
+led15=Pin(15,Pin.OUT)
+tim1=Timer()
+tim2=Timer()
+
+def tick(timer):
+    led14.toggle()
+def labell(timer):
+    led15.toggle()
+
+tim1.init(freq=20,mode=Timer.PERIODIC,callback=tick)  #執行緒1，透過init公式設定時間,可以在while迴圈運作時，同時跑這段程式，並且每隔0
+tim2.init(freq=1,mode=Timer.ONE_SHOT,callback=labell)                                     #.05秒再進行一次
+while True:
+    time.sleep(1)
+    led.toggle()
+
+
+
