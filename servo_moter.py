ca03d29f965bc58e222512758130c53a70707053
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-28 22:34:08 +0800

    111.10.18 thonny pico

--- servo_moter.py
+++ servo_moter.py
@@ -10,6 +10,6 @@ def servo(degrees):
     newDuty=minDuty+(maxDuty-minDuty)*(degrees/180)
     servoPin.duty_u16(int(newDuty))     #servoPin.duty_u16=0~65525
 while True:
-    for degree in range(0,180,1):
+    for degree in range(0,180,1):   #先轉到0度位置，再慢慢從1度,2度,3度....轉到179度，再一次繞一大圈回去0度
         servo(degree)
         sleep(0.01)
